702cb763497988869a3ecdb60114bdfb31b47e9e
Zuzanna <dsw52718@student.dsw.edu.pl>
2024-04-22 20:42:56 +0200

    dodanie skryptu zaktualizowanego

--- help.sh
+++ help.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
 show_help() {
-	echo "Użycie: $0 --help"
+	echo "Użycie: $0 [--help | -h]"
 }
 
-if [ "$1" == "--help" ]; then
+if [ "$1" == "--help" ] || [ "$1" == "-h"]; then
 	show_help
 	echo "Dostępne skrypty:"
 	echo "  data.sh   - Skrypt wyświetlający datę dzisiejszą."
 	echo "  logs.sh - Skrypt tworzący pliki (100)."
-	echo "  30.sh - Skrypt tworzący pliki (30)"
+	echo "  30.sh - Skrypt tworzący pliki (30)."
+	echo "  help.sh - Skrpyt wyświetlający dostępne opcje."
+	echo "  error.sh - Skrypt tworzący errorx/errorx.txt."
+	echo "  skrypt.sh - Skrypt klonujący repozytorium."
 	exit 0
 else
 	echo "Błąd: Nieznany argument."
