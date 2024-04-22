435811bd24925fec31e05f24aa1589284f92624a
Zuzanna <dsw52718@student.dsw.edu.pl>
2024-04-22 20:17:21 +0200

    dodano zmodyfikowane skrypty

---
+++ skrypt.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ "$1" == "--init" ]; then
+	git clone https://github.com/kr0cz4k/lab4 .
+
+	current_directory=$(pwd)
+	echo "export PATH=\$PATH:$current_directory" >> ~/.bashrc
+	echo "Repozytorium zostało sklonowane i ścieżka została ustawiona w PATH."
+	echo "Należy zrestartować terminal, aby zmiany zaczęły obowiązywać."
+	exit 0
+else
+	echo "Użycie: $0 --init"
+	exit 1
+fi
