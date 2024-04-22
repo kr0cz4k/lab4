d775ed50ef8b65873961f1458d9fa96025d7cb2c
Zuzanna <dsw52718@student.dsw.edu.pl>
2024-04-22 15:27:38 +0200

    dodanie skryptu

---
+++ logs.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ "$#" -ne 1 ]; then
+    echo "Użycie: $0 --logs"
+    exit 1
+fi
+
+if [ "$1" != "--logs" ]; then
+    echo "Użycie: $0 --logs"
+    exit 1
+fi
+
+for ((i=1; i<=100; i++)); do
+	filename="log$i.txt"
+	echo "Nazwa pliku: $filename" > "$filename"
+	echo "Nazwa skryptu: $0" >> "$filename"
+	echo "Data utworzenia: $(date)" >> "$filename"
+done
+
+echo "Utworzono 100 plików logx.txt"
