76407405f2525ce88edb7f720d185a1292c804a0
Zuzanna <dsw52718@student.dsw.edu.pl>
2024-04-22 20:35:40 +0200

    dodanie skryptu

--- data.sh
+++ data.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
+if [ "$#" -ne 1 ]; then
+	echo "Użycie: $0 [--data | -d]"
+	exit 1
+fi
+
+if [ "$1" != "--data" ] && [ "$1" != "-d" ]; then
+	echo "Użycie: $0 [--data | -d]"
+	exit 1
+fi
+
 dzisiaj=$(date +"%Y-%m-%d")
 
 echo "Dzisiaj jest: $dzisiaj"
-
